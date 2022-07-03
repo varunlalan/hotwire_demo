d16df6db2b6ad2e446dd74c883fad74fec431bad
Varun Lalan <varun.lalan@startv.com>
2022-07-03 14:49:05 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'other/index'
+
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
+
+  # Add this line below
+  post '/other/post_something' => 'other#post_something', as: 'post_something'
+
+  resources :quotes
+
+  root to: "home#index"
+end
